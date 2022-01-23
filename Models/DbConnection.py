f828a1de671ffec4115dc05f0a1044e69455f61d
Raúl Caro Pastorino <dev@fryntiz.es>
2022-01-24 00:02:55 +0100

    Added db class

--- Models/DbConnection.py
+++ Models/DbConnection.py
@@ -42,12 +42,200 @@
 # #           Descripción           # #
 #######################################
 ##
+# # Conexión a la base de datos.
 ##
 
 #######################################
 # #       Importar Librerías        # #
 #######################################
 
-#######################################
-# #            FUNCIONES            # #
-#######################################
+import datetime
+from sqlalchemy import create_engine, Table, Column, Integer, String, \
+                       MetaData, DateTime, Numeric, select, text
+
+from sqlalchemy.orm import sessionmaker
+
+## Cargo archivos de configuración desde .env
+from dotenv import load_dotenv
+load_dotenv(override=True)
+import os
+
+
+class Dbconnection:
+    has_debug = os.getenv("DEBUG") == "True"
+
+    # Datos desde .env
+    DB_CONNECTION = os.getenv("DB_CONNECTION")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_DATABASE = os.getenv("DB_DATABASE")
+    DB_USERNAME = os.getenv("DB_USERNAME")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+    # Conexión a la base de datos
+    engine = create_engine(DB_CONNECTION + '://' + DB_USERNAME +
+                           ':' + DB_PASSWORD + '@' + DB_HOST + ':' + DB_PORT
+                           + '/' + DB_DATABASE)
+    meta = MetaData()
+    connection = engine.connect()
+
+    # Sesión para acciones por lotes
+    Session = sessionmaker(bind=engine)
+
+    tables = {}
+
+    def table_set_new(self, tablename, parameters):
+        """
+        Almacena una nueva tabla en el array de tablas.
+        :param tablename: Nombre de la tabla.
+        :param parameters: Parámetros para cada columna.
+        """
+        columns = []
+
+        # Seteo la columna **id**
+        columns.append(Column('id', Integer, primary_key=True, autoincrement=True))
+
+        # Seteo el resto de columnas.
+        for name, datas in parameters.items():
+            data_type = datas['type']
+            data_params = datas['params']
+            type_column = None
+            other_data = datas['others']
+
+            # Creo el campo según el tipo de dato.
+            if data_type == 'Numeric':
+                type_column = Numeric(**data_params)
+            elif data_type == 'DateTime':
+                type_column = DateTime
+            elif data_type == 'Integer':
+                type_column = Integer
+            elif data_type == 'String':
+                type_column = String(**data_params)
+
+            if datas['others']:
+                columns.append(Column(name, type_column, **other_data))
+            else:
+                columns.append(Column(name, type_column))
+
+        # Creo la tabla con las columnas antes seteadas.
+        self.tables[tablename] = Table(
+            tablename,
+            self.meta,
+            *columns,
+        )
+
+        self.meta.create_all(self.engine)
+
+        print('Tablas en la DB: ', self.engine.table_names())
+
+    def table_get_data(self, tablename):
+        """
+        Obtiene los datos de una tabla previamente seteada.
+        :param tablename: Nombre de la tabla desde la que obtener datos.
+        """
+
+        table = self.tables[tablename]
+
+        # Ejecuto la consulta para traer las tuplas de la tabla completa
+        return self.connection.execute(
+            select([table])
+        ).fetchall()
+
+    def table_get_data_last(self, tablename, limit):
+        """
+        Obtiene los datos de una tabla previamente seteada limitando resultados.
+        :param tablename: Nombre de la tabla desde la que obtener datos.
+        :param limit: Límite de datos a extraer de la db
+        """
+
+        table = self.tables[tablename]
+
+        if self.has_debug:
+            print('----------- table_get_data_last ------------')
+
+        # Ejecuto la consulta para traer las tuplas de la tabla limitada
+        return self.connection.execute(
+            select([table]).order_by(text('created_at DESC')).limit(limit)
+        ).fetchall()
+
+    def table_save_data(self, sensorname, tablename, params):
+        """
+        Almacena datos recibidos en la tabla recibida.
+        :param sensorname: Nombre del sensor sobre el que se trabaja.
+        :param tablename: Nombre de la tabla en la que guardar.
+        :param params: Diccionario con los parámetros del sensor.
+        """
+
+        table = self.tables[tablename]
+
+        print('Guardando en DB: ', table, params)
+
+        # Inserto Datos
+        try:
+            stmt = table.insert().values(params).return_defaults()
+            result = self.connection.execute(stmt)
+            # server_created_at = result.returned_defaults['created_at']
+        except Exception as e:
+            print('Ha ocurrido un problema al insertar datos', e.__class__.__name__)
+            return None
+
+    def table_truncate(self, tablename):
+        """
+        Vacia completamente la tabla recibida.
+        :param tablename: Nombre de la tabla.
+        """
+        self.connection.execute(self.tables[tablename].delete())
+
+    def table_drop_last_elements(self, tablename, limit):
+        """
+        Elimina los últimos elementos en la cantidad recibida, de una
+        tabla recibida
+        :param tablename: Nombre de la tabla sobre la que actuar
+        :param limit: Límite, cantidad de entradas a borrar
+        :return:
+        """
+        table = self.tables.get(tablename)
+        session = self.Session()
+
+        ## Obtengo los últimos elementos para eliminarlos posteriormente.
+        lastData = self.table_get_data_last(tablename, limit)
+
+        ## Almaceno los ids de todos los resultados.
+        ids = []
+
+        for data in lastData:
+            ids.append(data.id)
+
+        query = table.delete().where(table.c.id.in_(ids))
+        session.execute(query)
+        session.commit()
+
+    def get_all_data(self):
+        '''
+        Obtiene todos los datos de la base de datos para todos los
+        sensores y los devuelve organizados.
+        '''
+        pass
+
+    def truncate_all_sensors_data(self):
+        """
+        Limpia todas las tablas para los sensores establecidos.
+        """
+        pass
+
+    def truncate_db(self):
+        """
+        Limpia la Base de datos completamente para comenzar a recopilar
+        información desde una base de datos saneada/limpia.
+        """
+        con = self.connection
+        trans = con.begin()
+        con.execute('SET FOREIGN_KEY_CHECKS = 0;')
+        for table in self.meta.sorted_tables:
+            con.execute(table.delete())
+        con.execute('SET FOREIGN_KEY_CHECKS = 1;')
+        trans.commit()
+
+    def close_connection(self):
+        print('Cerrando conexión con la Base de Datos')
+        self.connection.close()
